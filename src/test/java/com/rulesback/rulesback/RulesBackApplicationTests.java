bcd36508820803ef51e181d44e172dccdd054c9d
CamiloCJ09 <camilocampazj@gmail.com>
2022-12-06 15:43:58 -0500

    config proyect

--- src/test/java/com/rulesback/rulesback/RulesBackApplicationTests.java
+++ src/test/java/com/rulesback/rulesback/RulesBackApplicationTests.java
@@ -1,5 +1,7 @@
 package com.rulesback.rulesback;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,8 @@ class RulesBackApplicationTests {
 
     @Test
     void contextLoads() {
+        assertTrue(true);
+        System.out.println("Hello World");
     }
 
 }
