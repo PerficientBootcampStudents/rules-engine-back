9d76dc72be83fe2a0428bac048a66ed3d1935ed9
CamiloCJ09 <camilocampazj@gmail.com>
2022-12-06 15:31:12 -0500

    first commit

---
+++ src/main/java/com/rulesback/rulesback/RulesBackApplication.java
@@ -0,0 +1,13 @@
+package com.rulesback.rulesback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RulesBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RulesBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rulesback/rulesback/RulesBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rulesback.rulesback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RulesBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
